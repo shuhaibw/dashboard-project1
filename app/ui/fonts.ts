d59c7a136190135b9304534f85d01a79fceb8813
shuhaibw <120868621+shuhaibw@users.noreply.github.com>
2024-04-23 15:55:04 -0700

    Push project to github

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+//import the fonts
+import { Inter,Lusitana } from 'next/font/google';
+//set assessts from google fonts page
+export const inter = ({subsets:['latin']});
+export const lusitana = Lusitana({weight:['400','700'],subsets:['latin'],});
